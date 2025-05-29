13b2b84cf0bbe58f8900354c83206e9a876ac6ea
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-05-29 19:19:40 -0400

    Fix Equicord

--- scripts/getEquicordBadges.mjs
+++ scripts/getEquicordBadges.mjs
@@ -20,7 +20,7 @@ const getEquicordBadges = async () => {
         });
 
         const { data: donorData } = await axios.get(
-            "https://equicord.org/badges",
+            "https://equicord.org/badges.json",
             { headers: { "Cache-Control": "no-cache" } }
         );
 
